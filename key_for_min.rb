e271c1af060979ab6a259b7fe0c34edfeb9be225
Shadman Ahmed <shadman@outlook.com>
2020-05-21 05:11:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.find do |name, value|
- value.include? (name)
+  name_hash.find do |key, value|
+ value.include? (key)
 end
 end
